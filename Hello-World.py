d6d87664e37d8adbeef437fb1165ac85bbaaf16f
ZilongPan <lnginse@gmail.com>
2016-11-29 11:29:05 -0500

    Added two exclamation marks at the end

--- Hello-World.py
+++ Hello-World.py
@@ -2,4 +2,6 @@ Python 3.5.2 (v3.5.2:4def2a2901a5, Jun 25 2016, 22:01:18) [MSC v.1900 32 bit (In
 Type "copyright", "credits" or "license()" for more information.
 >>> print("hellow world")
 hellow world
->>> 
+>>> print('hellow world!!!')
+
+
